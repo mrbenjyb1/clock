02ccfa0f18d2c1998d34f297ebeaf86aa9cc00de
mrbenjyb1 <94203240+mrbenjyb1@users.noreply.github.com>
2021-11-13 00:43:35 +0000

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,29 @@
+#miror screen project
+
+
+
+from time import strftime
+from tkinter import Label, Tk
+
+#======= Configuring window =========
+window = Tk()
+window.title("time")
+window.geometry("800x480+80+0")
+window.attributes("-alpha", "0")
+window.resizable(False, False)
+#=========text info===========
+clock_label = Label(window,bg="silver", fg="white", font = ("calibri", 148, 'bold'), relief='flat')
+clock_label.place(x = 0, y = 0)
+window.overrideredirect(True)
+
+
+#=========displaying time========
+def update_label():
+    current_time = strftime('%H: %M: %S')
+    clock_label.configure(text = current_time)
+    clock_label.after(80, update_label)
+
+update_label()
+window.mainloop()
+
+
