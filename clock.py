cd63120b26cbdd39605159bff9c35d12acb14618
mrbenjyb1 <94203240+mrbenjyb1@users.noreply.github.com>
2021-11-23 23:10:50 +0000

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,6 +1,12 @@
-
+#=======import and setup========
 from time import strftime
 from tkinter import Label, Tk
+import os
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
+import matplotlib.pyplot as plt
 
 #======= Configuring window =========
 window = Tk()
