cd7d109d1d34981dcb7f22e93b792e68f45c8a39
mrbenjyb1 <94203240+mrbenjyb1@users.noreply.github.com>
2021-11-26 00:21:15 +0000

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,3 +1,4 @@
+
 #=======import and setup========
 from time import strftime
 from tkinter import Label, Tk
@@ -11,8 +12,8 @@ if os.environ.get('DISPLAY','') == '':
 window = Tk()
 window.title("time")
 window.geometry("800x480+80+0")
-window.attributes("-fullscreen", True)
-window.resizable(False, False)
+window.attributes("-fullscreen", False)
+window.resizable(True, True)
 #=========text info===========
 clock_label = Label(window,bg="black", fg="white", font = ("calibri", 148, 'bold'), relief='flat')
 clock_label.place(x = 0, y = 0)
@@ -20,12 +21,11 @@ window.overrideredirect(True)
 
 #=========displaying time========
 def update_label():
-    current_time = strftime('%H: %M: %S')
+    current_time = strftime('%H: %M')
     clock_label.configure(text = current_time)
     clock_label.after(80, update_label)
 #============main==============
 
 update_label()
 window.mainloop()
-
 
